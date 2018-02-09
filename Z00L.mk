dacfa44f8d4b3a6a18a5a2d85da90916bd563a8f
AKabhishek <abhishekkaushik.star@gmail.com>
2018-02-09 11:52:28 +0000

    ota: add Z00L
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

---
+++ Z00L.mk
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ROM>
+<RomName>Atomic-OS</RomName>
+<VersionName>Atomic-OS-PLASMA-2.3-Z00L-20180206-1632
+</VersionName>
+<VersionNumber type="integer">20180205</VersionNumber>
+<DirectUrl><![CDATA[https://excellmedia.dl.sourceforge.net/project/teamdarkness/Atomic-OS/Z00L/Atomic-OS-PLASMA-2.3-Z00L-20180206-1632.zip]]></DirectUrl>
+<HttpUrl><![CDATA[https://sourceforge.net/projects/teamdarkness/files/Atomic-OS/Z00L/Atomic-OS-PLASMA-2.3-Z00L-20180206-1632.zip/download]]></HttpUrl>
+<Android>8.1.0</Android>
+<CheckMD5>253fe1f1d52ad10c243ed9cbb4844642</CheckMD5>
+<FileSize type="integer">408177255</FileSize>
+<Developer><![CDATA[mg712702]]></Developer>
+<WebsiteURL><![CDATA[Add thread link here]]></WebsiteURL>
+<DonateURL><![CDATA[https://paypal.me/AbhishekAk]]></DonateURL>
+<Changelog>
+<![CDATA[
+# Note :
+_Clean flash is  recommended. Bug reports are not appreciated without Logcat.
+Builds will be available soon when maintainers will get free time. So don't ask for ETAs._
+---
+]]>
+</Changelog>
+</ROM>
