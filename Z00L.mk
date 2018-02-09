53018cba43ce8082ce6a769e77ea26af5aad076d
Mrinal Ghosh <31700322+mg712702@users.noreply.github.com>
2018-02-09 17:56:02 +0530

    First Release

--- Z00L.mk
+++ Z00L.mk
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <ROM>
 <RomName>Atomic-OS</RomName>
-<VersionName>Atomic-OS-PLASMA-2.3-Z00L-20180206-1632
+<VersionName>Atomic-OS-PLASMA-2.3-Z00L-20180209-0908
 </VersionName>
-<VersionNumber type="integer">20180205</VersionNumber>
-<DirectUrl><![CDATA[https://excellmedia.dl.sourceforge.net/project/teamdarkness/Atomic-OS/Z00L/Atomic-OS-PLASMA-2.3-Z00L-20180206-1632.zip]]></DirectUrl>
-<HttpUrl><![CDATA[https://sourceforge.net/projects/teamdarkness/files/Atomic-OS/Z00L/Atomic-OS-PLASMA-2.3-Z00L-20180206-1632.zip/download]]></HttpUrl>
+<VersionNumber type="integer">20180209</VersionNumber>
+<DirectUrl><![CDATA[https://excellmedia.dl.sourceforge.net/project/teamdarkness/Atomic-OS/Z00L/Atomic-OS-PLASMA-2.3-Z00L-20180209-0908.zip]]></DirectUrl>
+<HttpUrl><![CDATA[https://sourceforge.net/projects/teamdarkness/files/Atomic-OS/Z00L/Atomic-OS-PLASMA-2.3-Z00L-20180209-0908.zip/download]]></HttpUrl>
 <Android>8.1.0</Android>
-<CheckMD5>253fe1f1d52ad10c243ed9cbb4844642</CheckMD5>
-<FileSize type="integer">408177255</FileSize>
-<Developer><![CDATA[mg712702]]></Developer>
-<WebsiteURL><![CDATA[Add thread link here]]></WebsiteURL>
-<DonateURL><![CDATA[https://paypal.me/AbhishekAk]]></DonateURL>
+<CheckMD5>4385b4d959b41e9514944c8cefa5e9f7</CheckMD5>
+<FileSize type="integer">508177255</FileSize>
+<Developer><![CDATA[Mrinal Ghosh(mg712702)]]></Developer>
+<WebsiteURL><![CDATA[https://forum.xda-developers.com/zenfone-2-laser/development/rom-atomic-os-2-1-plasma-t3737041]]></WebsiteURL>
+<DonateURL><![CDATA[https://paypal.me/mg712702]]></DonateURL>
 <Changelog>
 <![CDATA[
 # Note :
